2acd9df514ad42f85ab4164f816313e6c4c21fa2
David Runge <dvzrv@archlinux.org>
2023-06-10 10:23:27 +0200

    feat: Derive Clone for PkgType

--- alpm-types/src/pkg.rs
+++ alpm-types/src/pkg.rs
@@ -108,7 +108,7 @@ impl Display for Packager {
 /// assert_eq!("src", format!("{}", PkgType::Source));
 /// assert_eq!("split", format!("{}", PkgType::Split));
 /// ```
-#[derive(Debug, Display, EnumString, PartialEq)]
+#[derive(Clone, Debug, Display, EnumString, PartialEq)]
 #[non_exhaustive]
 pub enum PkgType {
     /// a debug package
