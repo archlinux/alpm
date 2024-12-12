8fa0a46a462f2d36290cbafad996f9704fc11563
Arne Beer <contact@arne.beer>
2024-12-12 16:45:59 +0100

    feat(mtree): Format subcommand and json serialization
    Add a `Format` subcommand for serialization into structured data.
    For now, only json is supported.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-mtree/src/error.rs
+++ alpm-mtree/src/error.rs
@@ -32,4 +32,8 @@ pub enum Error {
     /// An error occurred during the interpretation phase of the language.
     #[error("Error while interpreting file in line {0}:\nAffected line:\n{1}\n\nReason:\n{2}")]
     InterpreterError(usize, String, String),
+
+    /// JSON error
+    #[error("JSON error: {0}")]
+    Json(#[from] serde_json::Error),
 }
