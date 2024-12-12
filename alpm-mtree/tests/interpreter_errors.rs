b7ed673d4a2638d1a6b649be81e37cd9f3907008
Arne Beer <contact@arne.beer>
2024-12-12 10:14:53 +0100

    test(mtree): interpreter errors
    Add test cases for interpreter errors.
    Make sure that any such error results in a helpful message that provides
    context to where the error occurred.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

---
+++ alpm-mtree/tests/interpreter_errors.rs
@@ -0,0 +1,48 @@
+use std::{fs::read_to_string, path::PathBuf};
+
+use alpm_mtree::validate;
+use insta::assert_snapshot;
+use rstest::rstest;
+use testresult::TestResult;
+
+/// Mtree files are processed in two steps:
+///
+/// 1. Parsing the file. Make sure that the syntax is correct and return the syntax representation.
+/// 2. Interpret the syntax. MTree files are stateful via the `/set` and `/unset` commands.
+///
+/// This test covers errors in the interpretation step.
+///
+/// Test that mtree files with missing or superfluous properties result in the expected error
+/// messages. The input files stored in the `interpreter_error_input` directory.
+/// The snapshot files are stored in the `interpreter_error_snapshots` directory under the
+/// respective filenames.
+#[rstest]
+pub fn ensure_errors_v1(#[files("tests/interpreter_error_inputs/*")] case: PathBuf) -> TestResult {
+    // Read the input file and parse it.
+    let input = read_to_string(&case)?;
+    let result = validate(Some(&case));
+
+    let Err(error) = result else {
+        return Err(format!(
+            "The interpreter succeeded even though it should've failed for input:\n{input}"
+        )
+        .into());
+    };
+
+    let name = case.file_stem().unwrap().to_str().unwrap();
+
+    // Run the tests with the input being displayed as the description.
+    // This makes reviewing this whole stuff a lot easier.
+    // Also remove the usual module prefix, as we're already manually sorting snapshots by test
+    // scenario.
+    let input_clone = input.clone();
+    insta::with_settings!({
+        description => input_clone,
+        snapshot_path => "interpreter_error_snapshots",
+        prepend_module_to_snapshot => false,
+    }, {
+        assert_snapshot!(name, error);
+    });
+
+    Ok(())
+}
