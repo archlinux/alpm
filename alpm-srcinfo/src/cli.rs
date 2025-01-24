5182ce33501b110cafe930ca210dcd350f8b5781
Arne Beer <contact@arne.beer>
2025-01-24 15:29:39 +0100

    feat(srcinfo): Add format command for MergedPackage representation
    Add structured output for the list of MergedPackages.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-srcinfo/src/cli.rs
+++ alpm-srcinfo/src/cli.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use alpm_types::Architecture;
 use clap::Parser;
 use clap::Subcommand;
 
@@ -8,6 +9,14 @@ use clap::Subcommand;
 pub struct Cli {
     #[command(subcommand)]
     pub command: Command,
+}
+
+/// Output format for the parse command
+#[derive(Clone, Debug, Default, clap::ValueEnum, strum::Display)]
+pub enum OutputFormat {
+    #[default]
+    #[strum(serialize = "json")]
+    Json,
 }
 
 #[allow(clippy::large_enum_variant)]
@@ -23,7 +32,41 @@ pub enum Command {
         #[arg(value_name = "FILE")]
         file: Option<PathBuf>,
     },
+    /// Format a SRCINFO file from a path or `stdin`
+    ///
+    /// Read, validate and print all of the SRCINFO's packages in their final representation for a
+    /// specific architecture. If the file is valid, the program prints the data in the
+    /// requested file format to stdout and returns with an exit status of 0.
+    /// architecture.
+    /// exits with the data returned in another file format on stdout and a return code of 0.
+    /// a non-zero exit status.
+    /// a non-zero exit code.
+    #[command()]
+    Format {
+        #[arg(value_name = "FILE")]
+        file: Option<PathBuf>,
 
+        /// The selected architecture that should be used to interpret the SRCINFO file.
+        ///
+        /// Only [split-]packages that are applicable for this architecture will be returned.
+        #[arg(short, long)]
+        architecture: Architecture,
+
+        /// Provide the output format
+        #[arg(
+            short,
+            long,
+            value_name = "OUTPUT_FORMAT",
+            default_value_t = OutputFormat::Json
+        )]
+        output_format: OutputFormat,
+
+        /// Determines whether the output will be displayed in a pretty non-minimized fashion.
+        ///
+        /// Only applies to formats that support pretty output and is otherwise ignored.
+        #[arg(short, long)]
+        pretty: bool,
+    },
     /// Read a SRCINFO file from a path or `stdin` and perform linter checks on it.
     ///
     /// This ensures that the SRCINFO file is both **valid** and adheres to currently known best
