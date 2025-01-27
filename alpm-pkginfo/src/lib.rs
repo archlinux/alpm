d77987055979c88dc37cfd16f18245e1d2d784f8
Arne Beer <contact@arne.beer>
2025-01-27 12:16:43 +0100

    feat(pkginfo): Add cli feature flag
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-pkginfo/src/lib.rs
+++ alpm-pkginfo/src/lib.rs
@@ -6,7 +6,9 @@ pub use crate::pkginfo_v1::PkgInfoV1;
 mod pkginfo_v2;
 pub use crate::pkginfo_v2::PkgInfoV2;
 
+#[cfg(feature = "cli")]
 pub mod cli;
+#[cfg(feature = "cli")]
 pub mod commands;
 
 mod error;
