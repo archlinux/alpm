69549d828aec56f8b6692f01e7ca38a588377537
David Runge <dvzrv@archlinux.org>
2025-03-15 16:57:06 +0100

    feat: Add `SourceInfo` as entry point for reading SRCINFO data
    Signed-off-by: David Runge <dvzrv@archlinux.org>
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-srcinfo/src/lib.rs
+++ alpm-srcinfo/src/lib.rs
@@ -12,6 +12,7 @@ pub mod source_info;
 pub use error::Error;
 pub use merged::MergedPackage;
 pub use source_info::{
+    SourceInfo,
     relation::RelationOrSoname,
     v1::{SourceInfoResult, SourceInfoV1},
 };
