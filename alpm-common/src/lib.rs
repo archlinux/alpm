5c449e413f230ec9a7c3a220ef5d2f6c15e424df
David Runge <dvzrv@archlinux.org>
2025-03-27 17:13:44 +0100

    feat: Add abstractions for metadata and script file names in packages
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- alpm-common/src/lib.rs
+++ alpm-common/src/lib.rs
@@ -1,4 +1,6 @@
 #![doc = include_str!("../README.md")]
 
+mod package;
 mod traits;
+pub use package::{INSTALL_SCRIPTLET_FILENAME, MetadataFileName};
 pub use traits::{metadata_file::MetadataFile, schema::FileFormatSchema};
---
+++ alpm-common/src/package.rs
@@ -0,0 +1,33 @@
+//! Data related to package file contents.
+
+/// The name of an [alpm-install-scriptlet] file in an [alpm-package].
+///
+/// [alpm-install-scriptlet]: https://alpm.archlinux.page/specifications/alpm-install-scriptlet.5.html
+/// [alpm-package]: https://alpm.archlinux.page/specifications/alpm-package.7.html
+pub const INSTALL_SCRIPTLET_FILENAME: &str = ".INSTALL";
+
+/// The name of a required metadata file in an [alpm-package].
+///
+/// [alpm-package]: https://alpm.archlinux.page/specifications/alpm-package.7.html
+#[derive(
+    strum::AsRefStr, Clone, Copy, Debug, strum::Display, Eq, strum::IntoStaticStr, PartialEq,
+)]
+pub enum MetadataFileName {
+    /// The [BUILDINFO] file.
+    ///
+    /// [BUILDINFO]: https://alpm.archlinux.page/specifications/BUILDINFO.5.html
+    #[strum(to_string = ".BUILDINFO")]
+    BuildInfo,
+
+    /// The [ALPM-MTREE] file.
+    ///
+    /// [ALPM-MTREE]: ahttps://alpm.archlinux.page/specifications/ALPM-MTREE.5.html
+    #[strum(to_string = ".MTREE")]
+    Mtree,
+
+    /// The [PKGINFO] file.
+    ///
+    /// [PKGINFO]: https://alpm.archlinux.page/specifications/PKGINFO.5.html
+    #[strum(to_string = ".PKGINFO")]
+    PackageInfo,
+}
