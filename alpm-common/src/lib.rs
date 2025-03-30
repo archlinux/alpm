00c72c4512233bd171bdbbc1da9340b686807a5a
David Runge <dvzrv@archlinux.org>
2025-03-30 18:41:14 +0200

    feat: Add functions to get relative file paths from input dirs
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- alpm-common/src/lib.rs
+++ alpm-common/src/lib.rs
@@ -1,6 +1,10 @@
 #![doc = include_str!("../README.md")]
 
+mod error;
 mod package;
+mod package_input;
 mod traits;
+pub use error::Error;
 pub use package::{INSTALL_SCRIPTLET_FILENAME, MetadataFileName};
+pub use package_input::{relative_data_files, relative_files};
 pub use traits::{metadata_file::MetadataFile, schema::FileFormatSchema};
