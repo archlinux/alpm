c9e62a4f831aa9855a75abf16063db4889ec0173
Orhun Parmaksız <orhun@archlinux.org>
2025-04-21 20:29:01 +0300

    feat: Add alpm-soname crate for soname provision/dependency lookup
    Adds a new crate "alpm-soname" along with binaries for looking up
    soname provisions and dependencies in package files based on the
    given lookup directory.
    
    Signed-off-by: Orhun Parmaksız <orhun@archlinux.org>

---
+++ alpm-soname/src/error.rs
@@ -0,0 +1,65 @@
+//! Error handling.
+
+use std::path::PathBuf;
+
+/// The error that can occur when working with the library.
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum Error {
+    /// IO path error
+    #[error("I/O error at path {path} while {context}:\n{source}")]
+    IoPathError {
+        /// The path at which the error occurred.
+        path: PathBuf,
+
+        /// The context in which the error occurred at `path`.
+        ///
+        /// This is meant to complete the sentence "I/O error at path {path} while ".
+        context: &'static str,
+
+        /// The source of the error.
+        source: std::io::Error,
+    },
+
+    /// I/O error while writing.
+    #[error("IO write error while {context}:\n{source}")]
+    IoWriteError {
+        /// The context in which the error occurred.
+        context: &'static str,
+
+        /// The source of the error.
+        source: std::io::Error,
+    },
+
+    /// I/O error while reading.
+    #[error("IO read error while {context}:\n{source}")]
+    IoReadError {
+        /// The context in which the error occurred.
+        context: &'static str,
+
+        /// The source of the error.
+        source: std::io::Error,
+    },
+
+    /// ALPM PKGINFO error
+    #[error(transparent)]
+    AlpmPkginfo(#[from] alpm_pkginfo::Error),
+
+    /// ALPM types error
+    #[error(transparent)]
+    AlpmType(#[from] alpm_types::Error),
+
+    /// ALPM package error
+    #[error(transparent)]
+    AlpmPackage(#[from] alpm_package::Error),
+
+    /// ELF format handling error
+    #[error("ELF format error while {context}:\n{source}")]
+    ElfError {
+        /// The context in which the error occurred.
+        context: &'static str,
+
+        /// The source of the error.
+        source: goblin::error::Error,
+    },
+}
