7b4b48bc641154f8593fc1afb6704ab27f6fc3d3
Arne Beer <contact@arne.beer>
2025-04-28 11:49:40 +0200

    feat: Use bridge script to export PKGBUILD variables
    Use the alpm-pkgbuild-bridge bash script, which sources a given PKGBUILD
    and exports any variables and functions inside of it in a custom format.
    
    Adds the `pkgbuild` crate, binary and a `srcinfo run-bridge` subcommand
    , which is very useful for debugging.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

---
+++ alpm-pkgbuild/src/main.rs
@@ -0,0 +1,51 @@
+#![doc = include_str!("../README.md")]
+
+use std::process::ExitCode;
+
+use alpm_pkgbuild::cli::{Cli, Command, SourceInfoCommand};
+use clap::Parser;
+use commands::run_bridge;
+use log::LevelFilter;
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+
+mod commands;
+
+/// The entry point for the alpm-pkgbuild binary.
+///
+/// Parse the cli arguments and call the respective alpm-pkgbuild command functions
+fn main() -> ExitCode {
+    let cli = Cli::parse();
+    init_logger(cli.verbosity);
+    let result = match cli.command {
+        Command::Srcinfo { subcommand } => match subcommand {
+            SourceInfoCommand::RunBridge { pkgbuild_path } => run_bridge(pkgbuild_path),
+        },
+    };
+
+    if let Err(error) = result {
+        eprintln!("{error}");
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
+    }
+}
+
+/// Initializes a global logger once.
+fn init_logger(verbosity: u8) {
+    let level = match verbosity {
+        0 => LevelFilter::Error,
+        1 => LevelFilter::Warn,
+        2 => LevelFilter::Info,
+        3 => LevelFilter::Debug,
+        _ => LevelFilter::Trace,
+    };
+
+    if let Err(err) = TermLogger::init(
+        level,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    ) {
+        eprintln!("Failed to initialize logger:\n{err}")
+    }
+}
