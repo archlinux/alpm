8a697c979a52ab6a56b8be58f32f0ab56dc228c4
Arne Beer <contact@arne.beer>
2025-04-28 11:49:40 +0200

    feat: Use bridge script to export PKGBUILD variables
    Use the alpm-pkgbuild-bridge bash script, which sources a given PKGBUILD
    and exports any variables and functions inside of it in a custom format.
    
    Adds the `pkgbuild` crate, binary and a `srcinfo run-bridge` subcommand
    , which is very useful for debugging.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

---
+++ alpm-pkgbuild/src/error.rs
@@ -0,0 +1,91 @@
+//! All error types that are exposed by this crate.
+
+use std::{path::PathBuf, string::FromUtf8Error};
+
+#[cfg(doc)]
+use alpm_srcinfo::SourceInfo;
+use thiserror::Error;
+
+/// The high-level error that can occur when using this crate.
+#[derive(Debug, Error)]
+pub enum Error {
+    /// ALPM types error.
+    #[error(transparent)]
+    AlpmType(#[from] alpm_types::Error),
+
+    /// UTF-8 parse error.
+    #[error(transparent)]
+    InvalidUTF8(#[from] FromUtf8Error),
+
+    /// IO error
+    #[error("I/O error while {context}:\n{source}")]
+    Io {
+        /// The context in which the error occurred.
+        ///
+        /// This is meant to complete the sentence "I/O error while ...".
+        context: &'static str,
+        /// The error source.
+        source: std::io::Error,
+    },
+
+    /// IO error with additional path info for more context.
+    #[error("I/O error at path {path:?} while {context}:\n{source}")]
+    IoPath {
+        /// The path at which the error occurred.
+        path: PathBuf,
+        /// The context in which the error occurred.
+        ///
+        /// This is meant to complete the sentence "I/O error at path $path while ...".
+        context: &'static str,
+        /// The error source
+        source: std::io::Error,
+    },
+
+    /// Invalid file encountered
+    #[error("Encountered invalid file for path {path:?}:\n{context}")]
+    InvalidFile {
+        /// The path of the file that's invalid
+        path: PathBuf,
+        /// The context in which the error occurred.
+        ///
+        /// This is meant to complete the sentence "Encountered invalid file for path ...".
+        context: &'static str,
+    },
+
+    /// The alpm-pkgbuild-bridge script could not be found in `$PATH`.
+    #[error("Could not find '{script_name}' script in $PATH:\n{source}")]
+    ScriptNotFound {
+        /// The name of the script that couldn't be found.
+        script_name: String,
+        /// The error source
+        source: which::Error,
+    },
+
+    /// The pkgbuild bridge script failed to be started.
+    #[error(
+        "Failed to {context} process to extract PKGBUILD:\nCommand: alpm-pkgbuild-bridge {parameters:?}\n{source}"
+    )]
+    ScriptError {
+        /// The context in which the error occurred.
+        ///
+        /// This is meant to complete the sentence "Failed to ...".
+        context: &'static str,
+        /// The parameters that were supplied to the script.
+        parameters: Vec<String>,
+        /// The error source
+        source: std::io::Error,
+    },
+
+    /// The pkgbuild bridge script errored with some log output.
+    #[error(
+        "Error during pkgbuild bridge execution:\nCommand: alpm-pkgbuild-bridge {parameters:?}\nstdout:{stdout}\n\nstderr:{stderr}"
+    )]
+    ScriptExecutionError {
+        /// The parameters that were supplied to the script.
+        parameters: Vec<String>,
+        /// The stdout of the failed command.
+        stdout: String,
+        /// The stderr of the failed command.
+        stderr: String,
+    },
+}
---
+++ alpm-pkgbuild/src/lib.rs
@@ -0,0 +1,6 @@
+#![doc = include_str!("../README.md")]
+
+pub mod bridge;
+#[cfg(feature = "cli")]
+pub mod cli;
+pub mod error;
