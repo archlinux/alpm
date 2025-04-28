6916aea122fd050462a2228a976e36845d9c7c7b
Arne Beer <contact@arne.beer>
2025-04-28 11:49:40 +0200

    feat: Bridge script to export PKGBUILD variables
    Signed-off-by: Arne Beer <contact@arne.beer>

---
+++ alpm-pkgbuild/src/cli.rs
@@ -0,0 +1,44 @@
+use std::path::PathBuf;
+
+use clap::{ArgAction, Parser, Subcommand, ValueEnum};
+
+#[derive(Clone, Debug, Parser)]
+#[command(about, author, name = "alpm-mtree", version)]
+pub struct Cli {
+    /// Verbose mode (-v, -vv, -vvv)
+    #[arg(short, long, action = ArgAction::Count)]
+    pub verbosity: u8,
+
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+/// Output format for the parse command
+#[derive(Clone, Debug, Default, ValueEnum, strum::Display)]
+pub enum OutputFormat {
+    #[default]
+    #[strum(serialize = "json")]
+    Json,
+}
+
+#[allow(clippy::large_enum_variant)]
+#[derive(Clone, Debug, Subcommand)]
+pub enum Command {
+    /// SRCINFO related actions.
+    Srcinfo {
+        #[command(subcommand)]
+        subcommand: SourceInfoCommand,
+    },
+}
+
+#[allow(clippy::large_enum_variant)]
+#[derive(Clone, Debug, Subcommand)]
+pub enum SourceInfoCommand {
+    /// Run the bridge script on a PKGBUILD file and print the raw and unfiltered output.
+    #[command()]
+    RunBridge {
+        /// Path to the PKGBUILD file the bridge should be run on.
+        #[arg(value_name = "PKGBUILD_PATH")]
+        pkgbuild_path: PathBuf,
+    },
+}
