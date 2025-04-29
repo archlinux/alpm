8d0abd53511be3c09b43976bf27eac96002455e2
Arne Beer <contact@arne.beer>
2025-04-29 16:59:09 +0200

    feat: PKGBUILD bridge parser
    Adds a parser that brings the output of the pkgbuild bridge script into
    an intermediate representation that can then be transformed into a
    proper SourceInfo struct.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-pkgbuild/src/commands.rs
+++ alpm-pkgbuild/src/commands.rs
@@ -2,7 +2,9 @@
 
 use std::path::PathBuf;
 
-use alpm_pkgbuild::{bridge::run_bridge_script, cli::OutputFormat, error::Error};
+use alpm_pkgbuild::{
+    bridge::{parser::BridgeOutput, run_bridge_script}, cli::OutputFormat, error::Error};
+use winnow::Parser;
 
 /// Run the bridge script on a `PKGBUILD` and return the output.
 ///
@@ -11,6 +13,19 @@ use alpm_pkgbuild::{bridge::run_bridge_script, cli::OutputFormat, error::Error};
 /// Returns an error if `run_bridge_script` fails.
 pub fn run_bridge(pkgbuild_path: PathBuf) -> Result<(), Error> {
     println!("{}", run_bridge_script(&pkgbuild_path)?);
+
+    Ok(())
+}
+
+/// Take a `PKGBUILD` and create a `SRCINFO` file from it.
+pub fn print_source_info(pkgbuild_path: PathBuf) -> Result<(), Error> {
+    let bridge_output = run_bridge_script(&pkgbuild_path)?;
+
+    let output = BridgeOutput::parser
+        .parse(&bridge_output)
+        .map_err(|err| Error::BridgeParseError(format!("{err}")))?;
+
+    println!("{output:#?}");
 
     Ok(())
 }
