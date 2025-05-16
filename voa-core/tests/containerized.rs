b6c3e41d1b054806398986fd89da6928bf156e24
Heiko Schaefer <heiko@schaefer.name>
2025-05-16 23:12:38 +0200

    test(voa-core): Generalize test setup code
    Signed-off-by: Heiko Schaefer <heiko@schaefer.name>

--- voa-core/tests/containerized.rs
+++ voa-core/tests/containerized.rs
@@ -1,6 +1,9 @@
 #![cfg(feature = "_containerized-integration-test")]
 
-use std::fs;
+use std::{
+    fs::{File, create_dir_all},
+    os::unix::fs::symlink,
+};
 
 use log::{debug, warn};
 use simplelog::{ColorChoice, Config, LevelFilter, TermLogger, TerminalMode};
@@ -22,14 +25,45 @@ fn init_logger() -> TestResult {
     Ok(())
 }
 
-/// List information about opaque verifiers in the system load paths
+/// Objects that can be created during test setup
+enum Thing {
+    Path(&'static str),
+    File(&'static str),
+    SymLink(&'static str, &'static str),
+}
+
+/// Set up a test environment
+fn setup(things: &[Thing]) -> std::io::Result<()> {
+    for t in things {
+        match t {
+            Thing::Path(p) => create_dir_all(p)?,
+            Thing::File(f) => {
+                File::create(f)?;
+            }
+            Thing::SymLink(from, to) => symlink(from, to)?,
+        }
+    }
+
+    Ok(())
+}
+
+/// List information about verifiers in the system load paths
 /// for os=arch, purpose=packages, context=default, technology=openpgp.
 #[test]
 fn list_verifiers() -> TestResult {
     init_logger()?;
 
-    fs::create_dir_all("/usr/local/share/voa/arch/packages/default/openpgp/")?;
-    fs::File::create("/usr/local/share/voa/arch/packages/default/openpgp/foo.pgp")?;
+    const SETUP: &[Thing] = &[
+        Thing::Path("/usr/local/share/voa/arch/packages/default/openpgp/"),
+        Thing::File("/usr/local/share/voa/arch/packages/default/openpgp/foo.pgp"),
+        Thing::Path("/etc/voa/arch/packages/default/openpgp/"),
+        Thing::SymLink(
+            "/usr/local/share/voa/arch/packages/default/openpgp/foo.pgp",
+            "/etc/voa/arch/packages/default/openpgp/foo.pgp",
+        ),
+    ];
+
+    setup(SETUP).expect("test setup");
 
     let voa = Voa::new(LoadPaths::System);
     let verifiers = voa.load(
