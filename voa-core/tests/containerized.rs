65854340c10dfb5e0156843e9d387a855fdfc526
Heiko Schaefer <heiko@schaefer.name>
2025-05-17 11:32:45 +0200

    test(voa-core): More integration testing
    Signed-off-by: Heiko Schaefer <heiko@schaefer.name>

--- voa-core/tests/containerized.rs
+++ voa-core/tests/containerized.rs
@@ -26,6 +26,7 @@ fn init_logger() -> TestResult {
 }
 
 /// Objects that can be created during test setup
+#[derive(Debug)]
 enum Thing {
     Path(&'static str),
     File(&'static str),
@@ -74,6 +75,70 @@ fn list_verifiers() -> TestResult {
     );
 
     warn!("Found verifiers: {:#?}", verifiers);
+
+    Ok(())
+}
+
+/// A symlink that points to a verifier outside the load paths
+#[test]
+fn invalid_verifier_symlink() -> TestResult {
+    init_logger()?;
+
+    const SETUP: &[Thing] = &[
+        Thing::Path("/usr/local/share/voa/arch/packages/default/openpgp/"),
+        Thing::File("/tmp/foo.pgp"),
+        Thing::SymLink(
+            "/tmp/foo.pgp",
+            "/usr/local/share/voa/arch/packages/default/openpgp/foo.pgp",
+        ),
+    ];
+
+    setup(SETUP).expect("test setup");
+
+    let voa = Voa::new(LoadPaths::System);
+    let verifiers = voa.load(
+        Os::new("arch".to_string(), None, None, None, None),
+        Purpose::new(Role::Packages, Mode::ArtifactVerifier),
+        Context::Default,
+        Technology::OpenPGP,
+    );
+
+    assert!(verifiers.is_empty());
+
+    Ok(())
+}
+
+/// VOA setup with a symlink that intermittently escapes outside the load paths
+#[test]
+fn invalid_dir_symlink() -> TestResult {
+    init_logger()?;
+
+    const SETUP: &[Thing] = &[
+        Thing::Path("/usr/local/share/voa/arch/packages/"),
+        Thing::Path("/tmp/default/"),
+        Thing::SymLink(
+            "/tmp/default/",
+            "/usr/local/share/voa/arch/packages/default",
+        ),
+        Thing::Path("/etc/voa/arch/packages/default/openpgp/"),
+        Thing::SymLink(
+            "/etc/voa/arch/packages/default/openpgp/",
+            "/tmp/default/openpgp",
+        ),
+        Thing::File("/etc/voa/arch/packages/default/openpgp/foo.pgp"),
+    ];
+
+    setup(SETUP).expect("test setup");
+
+    let voa = Voa::new(LoadPaths::System);
+    let verifiers = voa.load(
+        Os::new("arch".to_string(), None, None, None, None),
+        Purpose::new(Role::Packages, Mode::ArtifactVerifier),
+        Context::Default,
+        Technology::OpenPGP,
+    );
+
+    assert!(verifiers.is_empty());
 
     Ok(())
 }
