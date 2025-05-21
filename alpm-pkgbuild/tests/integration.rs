7d075742606a8e106371b9dffeabbfa061b85657
Arne Beer <contact@arne.beer>
2025-05-21 14:10:22 +0200

    feat: Add pkgbuild srcinfo comparison command
    Add a convenience and debug feature to compare the generated SRCINFO
    file from a PKGBUILD with an already existing .SRCINFO file.
    
    This change **only** tests logical equality. It doesn't compare the
    actual .SRCINFO output.
    
    Useful for ensuring backwards compatibility and debugging any
    differences on a logical level.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-pkgbuild/tests/integration.rs
+++ alpm-pkgbuild/tests/integration.rs
@@ -5,6 +5,7 @@ use assert_cmd::Command;
 use testresult::TestResult;
 
 const TEST_PKGBUILD: &str = include_str!("test_files/normal.pkgbuild");
+const TEST_SRCINFO: &str = include_str!("test_files/normal.srcinfo");
 
 mod srcinfo_run_bridge {
     use tempfile::tempdir;
@@ -73,6 +74,56 @@ mod srcinfo_format {
         let srcinfo = SourceInfoV1::from_string(&output)?.source_info()?;
 
         assert_eq!(srcinfo.base.name.inner(), "example");
+
+        Ok(())
+    }
+}
+
+mod srcinfo_compare {
+    use tempfile::tempdir;
+
+    use super::*;
+
+    /// Run the `srcinfo compare` subcommand to compare the generated .SRCINFO file
+    /// from a PKGBUILD with an existing .SRCINFO file.
+    #[test]
+    fn compare() -> TestResult {
+        // Write the PKGBUILD to a temporary directory
+        let mut tempdir = tempdir()?;
+        let pkgbuild_path = tempdir.path().join("PKGBUILD");
+        let mut file = File::create_new(&pkgbuild_path)?;
+        file.write_all(TEST_PKGBUILD.as_bytes())?;
+
+        // Write the .SRCINFO to a temporary directory
+        let srcinfo_path = tempdir.path().join(".SRCINFO");
+        let mut file = File::create_new(&srcinfo_path)?;
+        file.write_all(TEST_SRCINFO.as_bytes())?;
+
+        // Call the bridge on the that PKGBUILD file.
+        let mut cmd = Command::cargo_bin("alpm-pkgbuild")?;
+        cmd.args(vec![
+            "srcinfo",
+            "compare",
+            "--pkgbuild",
+            "PKGBUILD",
+            "--srcinfo",
+            ".SRCINFO",
+        ]);
+
+        cmd.current_dir(tempdir.path());
+
+        // Make sure the command was successful, which implies that the file matched!.
+        let assert = cmd.assert();
+        // Disable the cleanup so that users may investigate the output files.
+        if let Err(err) = assert.try_success() {
+            println!(
+                "Output files of this test can be found at: {}",
+                tempdir.path().to_string_lossy()
+            );
+            tempdir.disable_cleanup(true);
+            // Now we may error.
+            return Err(err.into());
+        }
 
         Ok(())
     }
