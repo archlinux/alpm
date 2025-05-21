fd41d55b14232079d233ba91e5ef7a8ce277cc19
Arne Beer <contact@arne.beer>
2025-05-21 14:10:22 +0200

    feat: Add pkgbuild srcinfo comparison command
    Add a convenience and debug feature to compare the generated SRCINFO
    file from a PKGBUILD with an already existing .SRCINFO file.
    
    This change **only** tests logical equality. It doesn't compare the
    actual .SRCINFO output.
    
    Useful for ensuring backwards compatibility and debugging any
    differences on a logical level.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-pkgbuild/src/error.rs
+++ alpm-pkgbuild/src/error.rs
@@ -53,6 +53,10 @@ pub enum Error {
     #[error("{0}")]
     BridgeConversionError(#[from] BridgeError),
 
+    /// A SourceInfo file could not be read.
+    #[error("JSON error: {0}")]
+    SourceInfoRead(#[from] alpm_srcinfo::error::Error),
+
     /// JSON error while creating JSON formatted output.
     ///
     /// This error only occurs when running the `commands` functions.
--- alpm-pkgbuild/src/main.rs
+++ alpm-pkgbuild/src/main.rs
@@ -2,7 +2,7 @@ use std::process::ExitCode;
 
 use clap::Parser;
 use cli::SourceInfoCommand;
-use commands::{print_source_info, run_bridge};
+use commands::{compare_source_info, print_source_info, run_bridge};
 use log::LevelFilter;
 use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
 
@@ -25,6 +25,10 @@ fn main() -> ExitCode {
                 pretty,
             } => print_source_info(pkgbuild_path, output_format, pretty),
             SourceInfoCommand::RunBridge { pkgbuild_path } => run_bridge(pkgbuild_path),
+            SourceInfoCommand::Compare {
+                pkgbuild_path,
+                srcinfo_path,
+            } => compare_source_info(pkgbuild_path, srcinfo_path),
         },
     };
 
