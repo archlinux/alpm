68fb00d3cb0d74283d194b478df7424c5660d412
Arne Beer <contact@arne.beer>
2025-05-21 14:10:22 +0200

    feat: Add pkgbuild srcinfo comparison command
    Add a convenience and debug feature to compare the generated SRCINFO
    file from a PKGBUILD with an already existing .SRCINFO file.
    
    This change **only** tests logical equality. It doesn't compare the
    actual .SRCINFO output.
    
    Useful for ensuring backwards compatibility and debugging any
    differences on a logical level.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-pkgbuild/src/commands.rs
+++ alpm-pkgbuild/src/commands.rs
@@ -2,11 +2,12 @@
 
 use std::path::PathBuf;
 
+use alpm_common::MetadataFile;
 use alpm_pkgbuild::{
     bridge::{parser::BridgeOutput, run_bridge_script},
     error::Error,
 };
-use alpm_srcinfo::SourceInfoV1;
+use alpm_srcinfo::{SourceInfo, SourceInfoV1};
 
 use crate::cli::OutputFormat;
 
@@ -39,6 +40,45 @@ pub fn print_source_info(
         OutputFormat::Srcinfo => {
             println!("{}", source_info.as_srcinfo())
         }
+    }
+
+    Ok(())
+}
+
+/// Run the bridge script on a `PKGBUILD` and return the output.
+///
+/// This is a development and debug command to better understand the inner workings of this binary.
+///
+/// If the generated and read SRCINFO representations are not equal, place two files into the
+/// current directory: `srcinfo.json` and `pkgbuild.json`.
+///
+/// These files contain pretty-printed json, which accurately depict the internal representation
+/// used to compare the two files.
+pub fn compare_source_info(pkgbuild_path: PathBuf, srcinfo_path: PathBuf) -> Result<(), Error> {
+    let output = BridgeOutput::from_file(&pkgbuild_path)?;
+    let pkgbuild_source_info: SourceInfoV1 = output.try_into()?;
+
+    let source_info = SourceInfo::from_file_with_schema(srcinfo_path, None)?;
+    let SourceInfo::V1(source_info) = source_info;
+
+    if source_info != pkgbuild_source_info {
+        let pkgbuild_source_info = serde_json::to_string_pretty(&pkgbuild_source_info)?;
+        let source_info = serde_json::to_string_pretty(&source_info)?;
+
+        let pkgbuild_json_path = PathBuf::from("pkgbuild.json");
+        std::fs::write("pkgbuild.json", pkgbuild_source_info).map_err(|source| {
+            Error::IoPath(pkgbuild_json_path, "writing pkgbuild.json file", source)
+        })?;
+        let srcinfo_json_path = PathBuf::from("srcinfo.json");
+        std::fs::write("srcinfo.json", source_info).map_err(|source| {
+            Error::IoPath(srcinfo_json_path, "writing srcinfo.json file", source)
+        })?;
+
+        eprintln!("Generated .SRCINFO content differs to .SRCINFO read from disk.");
+        eprintln!("Compare the two generated files srcinfo.json and pkgbuild.json for details");
+        std::process::exit(1);
+    } else {
+        println!("The generated content matches that read from the disk.");
     }
 
     Ok(())
