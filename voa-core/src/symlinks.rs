3032f4fd29ab09217d8b5520647f0ef6bd57d1df
Heiko Schaefer <heiko@schaefer.name>
2025-05-30 22:06:37 +0200

    feat(voa-core): Implement more VOA semantics, add more tests
    Signed-off-by: Heiko Schaefer <heiko@schaefer.name>

---
+++ voa-core/src/symlinks.rs
@@ -0,0 +1,182 @@
+//! Helper functionality for handling symlinks in VOA, and checking their validity within the
+//! VOA linking rules.
+
+use std::{
+    collections::HashSet,
+    fs::read_link,
+    path::{Path, PathBuf},
+};
+
+use log::{trace, warn};
+
+use crate::{
+    load_path::LoadPath,
+    types::{Error, VerifierSourcePath},
+};
+
+pub(crate) enum ResolveSym {
+    Masked,
+    File(PathBuf),
+    Dir(PathBuf),
+}
+
+/// Check and resolve an arbitrarily long chain of symlinks.
+///
+/// Can detect cycles in symlink chains and will return an Error in case of a cycle.
+/// Detects masking (symlinks to `/dev/null`) and returns `ResolveSym::Masked` for this case.
+///
+/// For results with the variants `ResolveSym::File` or `ResolveSym::Dir`, the returned `PathBuf`
+/// contains a fully canonicalized path.
+pub(crate) fn resolve_symlink(
+    start: &Path,
+    legal_symlink_paths: &[&LoadPath],
+) -> Result<ResolveSym, Error> {
+    if !start.is_symlink() {
+        warn!("⤷ Not a symlink {start:?}");
+
+        // This is actually an inconsistent call for this function - use a separate error?
+        return Err(Error::IllegalSymlink);
+    }
+
+    let mut path = start.to_path_buf();
+
+    // Remember all paths we've traversed to do symlink cycle detection
+    let mut paths_seen = HashSet::new();
+    paths_seen.insert(path.clone());
+
+    // Loop through chains of symlinks.
+    // Check legality of each intermediate hop!
+    loop {
+        let link_target = read_link(&path).unwrap(); // FIXME: map to our error
+
+        // Are we in a symlink cycle?
+        if paths_seen.contains(&link_target) {
+            return Err(Error::IllegalSymlink); // FIXME: signal cycle
+        }
+
+        if link_target.as_path().to_str() == Some("/dev/null") {
+            // Individual _signature verifiers_ may be masked using a symlink to `/dev/null`,
+            // independent of [technology].
+
+            return Ok(ResolveSym::Masked);
+        } else {
+            // Check that target file path is legal
+
+            // Symlinks may only point into locations under
+            // legal_symlink_paths
+            if !legal_symlink_paths
+                .iter()
+                .any(|p| link_target.starts_with(&p.path))
+            {
+                trace!("⤷ Illegal symlink target"); // FIXME: message should go in error
+                return Err(Error::IllegalSymlink);
+            }
+
+            let meta = match std::fs::symlink_metadata(&link_target) {
+                Ok(meta) => meta,
+                Err(err) => {
+                    warn!("⤷ Cannot get metadata of {link_target:?}");
+                    return Err(err.into());
+                }
+            };
+
+            // set up next loop iteration
+            let file_type = meta.file_type();
+            if file_type.is_file() {
+                return Ok(ResolveSym::File(link_target));
+            } else if file_type.is_dir() {
+                return Ok(ResolveSym::Dir(link_target));
+            } else if !file_type.is_symlink() {
+                warn!("Unexpected file type {file_type:?} for {link_target:?}");
+                return Err(Error::IllegalSymlink);
+            }
+
+            path = link_target;
+            paths_seen.insert(path.clone());
+        }
+    }
+}
+
+impl VerifierSourcePath {
+    /// Canonicalize a `VerifierSourcePath`, and check that its elements conform to VOA
+    /// restrictions.
+    ///
+    /// Ensures, that the provided [`VerifierSourcePath`] represents a legal path on the local
+    /// filesystem, and any involved symlinks conform to the VOA symlink restrictions.
+    ///
+    /// Checks the legality of symlinks (if any) in the VOA path structure, and
+    /// returns the canonicalized path to the target directory.
+    pub(crate) fn check_and_canonicalize(
+        &self,
+        legal_symlink_paths: &[&LoadPath],
+    ) -> std::io::Result<PathBuf> {
+        // Canonicalized base load path
+        // (any potential internal symlinks of this top level "load_path" are not checked)
+        let base_path = self.load_path().path.canonicalize()?;
+
+        /// Append a segment to a path and ensure that the resulting path conforms
+        /// to the VOA symlink constraints.
+        fn append(
+            current_path: &Path,
+            segment: &Path,
+            legal_symlink_paths: &[&LoadPath],
+        ) -> std::io::Result<PathBuf> {
+            let mut buf = current_path.join(segment);
+
+            if buf.is_symlink() {
+                match resolve_symlink(&buf, legal_symlink_paths) {
+                    Ok(resolved) => {
+                        match resolved {
+                            ResolveSym::Dir(dir) => buf = dir,
+                            ResolveSym::File(path) => {
+                                warn!("⤷ Found unexpected file at {path:?}");
+                                return Err(std::io::Error::other(format!(
+                                    "Unexpected file at {path:?}"
+                                )));
+                            }
+                            ResolveSym::Masked => {
+                                // VOA implementations must not consider masking symlinks for
+                                // directories and should raise a warning for them.
+
+                                warn!("⤷ Found illegal masking symlink at directory {buf:?}");
+                                return Err(std::io::Error::other(format!(
+                                    "Illegal masking symlink at directory {buf:?}"
+                                )));
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        warn!("⤷ Cannot resolve symlinks for {:?}: {err:?}", &buf);
+                        return Err(std::io::Error::other(format!(
+                            "{segment:?} is not valid following {buf:?}"
+                        )));
+                    }
+                }
+            }
+
+            if !buf.is_dir() {
+                trace!("CheckedVerifierSourcePath::new {buf:?} is not a directory");
+
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::NotADirectory,
+                    format!("{buf:?} is not a directory"),
+                ));
+            }
+
+            Ok(buf)
+        }
+
+        let mut path = append(&base_path, &self.os().path_segment(), legal_symlink_paths)?;
+        path = append(&path, &self.purpose().path_segment(), legal_symlink_paths)?;
+        path = append(&path, &self.context().path_segment(), legal_symlink_paths)?;
+        path = append(
+            &path,
+            &self.technology().path_segment(),
+            legal_symlink_paths,
+        )?;
+
+        trace!("CheckedVerifierSourcePath::new canonicalized path: {path:?}");
+
+        Ok(path)
+    }
+}
--- voa-core/tests/containerized.rs
+++ voa-core/tests/containerized.rs
@@ -1,7 +1,9 @@
 #![cfg(feature = "_containerized-integration-test")]
 
 use std::{
+    ffi::OsStr,
     fs::{File, create_dir_all},
+    io::{Read, Write},
     os::unix::fs::symlink,
 };
 
@@ -9,7 +11,6 @@ use log::{debug, warn};
 use simplelog::{ColorChoice, Config, LevelFilter, TermLogger, TerminalMode};
 use testresult::TestResult;
 use voa_core::{
-    LoadPaths,
     Voa,
     types::{Context, CustomContext, Mode, Os, Purpose, Role, Technology},
 };
@@ -73,9 +74,9 @@ fn list_verifiers() -> TestResult {
 
     setup(SETUP).expect("test setup");
 
-    let voa = Voa::new(LoadPaths::System);
-    let verifiers = voa.load(
-        Os::new("arch".to_string(), None, None, None, None),
+    let voa = Voa::init();
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
         Purpose::new(Role::Packages, Mode::ArtifactVerifier),
         Context::Default,
         Technology::OpenPGP,
@@ -104,9 +105,9 @@ fn invalid_verifier_symlink() -> TestResult {
 
     setup(SETUP).expect("test setup");
 
-    let voa = Voa::new(LoadPaths::System);
-    let verifiers = voa.load(
-        Os::new("arch".to_string(), None, None, None, None),
+    let voa = Voa::init();
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
         Purpose::new(Role::Packages, Mode::ArtifactVerifier),
         Context::Default,
         Technology::OpenPGP,
@@ -144,18 +145,246 @@ fn invalid_dir_symlink() -> TestResult {
 
     setup(SETUP).expect("test setup");
 
-    let voa = Voa::new(LoadPaths::System);
+    let voa = Voa::init();
 
     // Try to load the verifier in the "custom" context, which will fail
     // because the symlink setup is invalid
-    let verifiers = voa.load(
-        Os::new("arch".to_string(), None, None, None, None),
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
         Purpose::new(Role::Packages, Mode::ArtifactVerifier),
         Context::Custom(CustomContext::new("custom".into()).expect("custom context")),
         Technology::OpenPGP,
     );
 
     assert!(verifiers.is_empty());
+
+    Ok(())
+}
+
+/// VOA setup with a symlink that masks a verifier.
+/// All copies of verifiers with that name should not be returned as a result.
+#[test]
+fn masking() -> TestResult {
+    init_logger()?;
+
+    const SETUP: &[TestObject] = &[
+        TestObject::Path("/etc/voa/arch/packages/default/openpgp/"),
+        TestObject::File("/etc/voa/arch/packages/default/openpgp/foo.pgp"),
+        TestObject::File("/etc/voa/arch/packages/default/openpgp/bar.pgp"),
+        TestObject::Path("/usr/local/share/voa/arch/packages/default/openpgp/"),
+        TestObject::File("/usr/local/share/voa/arch/packages/default/openpgp/foo.pgp"),
+        TestObject::Path("/run/voa/arch/packages/default/openpgp/"),
+        TestObject::SymLink(
+            "/dev/null",
+            "/run/voa/arch/packages/default/openpgp/foo.pgp",
+        ),
+    ];
+
+    setup(SETUP).expect("test setup");
+
+    let voa = Voa::init();
+
+    // Try to load verifiers, which should return no results because there is a masking
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
+        Purpose::new(Role::Packages, Mode::ArtifactVerifier),
+        Context::Default,
+        Technology::OpenPGP,
+    );
+
+    assert_eq!(verifiers.len(), 1);
+    assert_eq!(verifiers[0].path().file_name(), Some(OsStr::new("bar.pgp")));
+
+    Ok(())
+}
+
+/// VOA setup with a file that is linked via a chain of two symlinks.
+#[test]
+fn symlink_multihop_file() -> TestResult {
+    init_logger()?;
+
+    const SETUP: &[TestObject] = &[
+        TestObject::Path("/etc/voa/arch/packages/default/openpgp/"),
+        TestObject::Path("/etc/voa/arch/packages/custom1/openpgp/"),
+        TestObject::Path("/etc/voa/arch/packages/custom2/openpgp/"),
+        TestObject::File("/etc/voa/arch/packages/default/openpgp/foo.pgp"),
+        TestObject::SymLink(
+            "/etc/voa/arch/packages/default/openpgp/foo.pgp",
+            "/etc/voa/arch/packages/custom1/openpgp/foo.pgp",
+        ),
+        TestObject::SymLink(
+            "/etc/voa/arch/packages/custom1/openpgp/foo.pgp",
+            "/etc/voa/arch/packages/custom2/openpgp/foo.pgp",
+        ),
+    ];
+
+    setup(SETUP).expect("test setup");
+
+    let voa = Voa::init();
+
+    // Try to load verifiers via "custom2", which should return foo.pgp,
+    // which points to the default context via an intermediate hop in the "custom1" context.
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
+        Purpose::new(Role::Packages, Mode::ArtifactVerifier),
+        Context::Custom(CustomContext::new("custom2".into()).expect("custom context")),
+        Technology::OpenPGP,
+    );
+
+    assert_eq!(verifiers.len(), 1);
+    assert_eq!(verifiers[0].path().file_name(), Some(OsStr::new("foo.pgp")));
+
+    // TODO: assert that we found the full canonicalized path
+
+    Ok(())
+}
+
+/// VOA setup with a cycle of two file-symlinks.
+/// Will not terminate without cycle detection!
+#[test]
+fn symlink_cycle_file() -> TestResult {
+    init_logger()?;
+
+    const SETUP: &[TestObject] = &[
+        TestObject::Path("/etc/voa/arch/packages/custom1/openpgp/"),
+        TestObject::Path("/etc/voa/arch/packages/custom2/openpgp/"),
+        TestObject::SymLink(
+            "/etc/voa/arch/packages/custom2/openpgp/foo.pgp",
+            "/etc/voa/arch/packages/custom1/openpgp/foo.pgp",
+        ),
+        TestObject::SymLink(
+            "/etc/voa/arch/packages/custom1/openpgp/foo.pgp",
+            "/etc/voa/arch/packages/custom2/openpgp/foo.pgp",
+        ),
+    ];
+
+    setup(SETUP).expect("test setup");
+
+    let voa = Voa::init();
+
+    // Try to load verifiers via "custom2", which should only find a symlink loop and return empty.
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
+        Purpose::new(Role::Packages, Mode::ArtifactVerifier),
+        Context::Custom(CustomContext::new("custom2".into()).expect("custom context")),
+        Technology::OpenPGP,
+    );
+
+    assert!(verifiers.is_empty());
+
+    Ok(())
+}
+
+/// VOA setup with a directory that is linked via a chain of two symlinks.
+#[test]
+fn symlink_multihop_dir() -> TestResult {
+    init_logger()?;
+
+    const SETUP: &[TestObject] = &[
+        TestObject::Path("/etc/voa/arch/packages/default/openpgp/"),
+        TestObject::Path("/etc/voa/arch/packages/custom1/"),
+        TestObject::Path("/etc/voa/arch/packages/custom2/"),
+        TestObject::File("/etc/voa/arch/packages/default/openpgp/foo.pgp"),
+        TestObject::SymLink(
+            "/etc/voa/arch/packages/default/openpgp",
+            "/etc/voa/arch/packages/custom1/openpgp",
+        ),
+        TestObject::SymLink(
+            "/etc/voa/arch/packages/custom1/openpgp",
+            "/etc/voa/arch/packages/custom2/openpgp",
+        ),
+    ];
+
+    setup(SETUP).expect("test setup");
+
+    let voa = Voa::init();
+
+    // Try to load verifiers via "custom2", which should return foo.pgp,
+    // which points to the default context via an intermediate hop in the "custom1" context.
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
+        Purpose::new(Role::Packages, Mode::ArtifactVerifier),
+        Context::Custom(CustomContext::new("custom2".into()).expect("custom context")),
+        Technology::OpenPGP,
+    );
+
+    assert_eq!(verifiers.len(), 1);
+    assert_eq!(verifiers[0].path().file_name(), Some(OsStr::new("foo.pgp")));
+
+    // TODO: assert that we found the full canonicalized path
+
+    Ok(())
+}
+
+/// VOA setup with a cycle of two dir-symlinks.
+/// Will not terminate without cycle detection!
+#[test]
+fn symlink_cycle_dir() -> TestResult {
+    init_logger()?;
+
+    const SETUP: &[TestObject] = &[
+        TestObject::Path("/etc/voa/arch/packages/custom1/"),
+        TestObject::Path("/etc/voa/arch/packages/custom2/"),
+        TestObject::SymLink(
+            "/etc/voa/arch/packages/custom2/openpgp",
+            "/etc/voa/arch/packages/custom1/openpgp",
+        ),
+        TestObject::SymLink(
+            "/etc/voa/arch/packages/custom1/openpgp",
+            "/etc/voa/arch/packages/custom2/openpgp",
+        ),
+    ];
+
+    setup(SETUP).expect("test setup");
+
+    let voa = Voa::init();
+
+    // Try to load verifiers via "custom2", which should only find a symlink loop and return empty.
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
+        Purpose::new(Role::Packages, Mode::ArtifactVerifier),
+        Context::Custom(CustomContext::new("custom2".into()).expect("custom context")),
+        Technology::OpenPGP,
+    );
+
+    assert!(verifiers.is_empty());
+
+    Ok(())
+}
+
+/// VOA setup with a verifier that contains data.
+///
+/// We find the verifier, read it, and check that it contains the expected data.
+#[test]
+fn read_verifier() -> TestResult {
+    init_logger()?;
+
+    // Set up the verifier and write data into it
+    const SETUP: &[TestObject] = &[TestObject::Path("/etc/voa/arch/packages/default/openpgp/")];
+
+    setup(SETUP).expect("test setup");
+
+    let mut file = File::create("/etc/voa/arch/packages/default/openpgp/foo.pgp").unwrap();
+    file.write_all(b"hello world").unwrap();
+
+    // Find the verifier and read its data
+    let voa = Voa::init();
+
+    let verifiers = voa.lookup(
+        Os::new("arch".to_string(), None, None, None, None).unwrap(),
+        Purpose::new(Role::Packages, Mode::ArtifactVerifier),
+        Context::Default,
+        Technology::OpenPGP,
+    );
+
+    assert_eq!(verifiers.len(), 1);
+
+    let mut verifier = &verifiers[0].open().expect("open file");
+
+    let mut s = String::new();
+    let _ = verifier.read_to_string(&mut s).expect("read file");
+
+    assert_eq!(s, "hello world");
 
     Ok(())
 }
