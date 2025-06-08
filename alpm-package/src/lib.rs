8f70cf65daddb2429d888ae458b13712bddf436d
David Runge <dvzrv@archlinux.org>
2025-06-08 17:07:36 +0200

    docs: Add initial library documentation
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- alpm-package/src/lib.rs
+++ alpm-package/src/lib.rs
@@ -1,1 +1,3 @@
-
+//! A library for handling [alpm-package]s.
+//!
+//! [alpm-package]: https://alpm.archlinux.page/specifications/alpm-package.7.html
--- alpm-state-repo/src/lib.rs
+++ alpm-state-repo/src/lib.rs
@@ -1,1 +1,3 @@
-
+//! A library for handling an [alpm-state-repo].
+//!
+//! [alpm-state-repo]: https://alpm.archlinux.page/specifications/alpm-state-repo.7.html
