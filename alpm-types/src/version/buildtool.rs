0d845535f26949073d74eeb73f53844cf97c37fb
David Runge <dvzrv@archlinux.org>
2025-06-28 16:06:04 +0200

    refactor!: Turn `BuildToolVersion` into an enum
    Properly track the makepkg version representation using a
    `MinimalVersion` and the pkgctl representation using a `FullVersion`.
    
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- alpm-types/src/version/buildtool.rs
+++ alpm-types/src/version/buildtool.rs
@@ -7,78 +7,139 @@ use std::{
 
 use serde::Serialize;
 
-use crate::{Architecture, Error, Version};
+#[cfg(doc)]
+use crate::BuildTool;
+use crate::{Architecture, Error, FullVersion, MinimalVersion, Version};
 
-/// The version and architecture of a build tool
+/// The version and optional architecture of a build tool.
 ///
-/// `BuildToolVersion` is used in conjunction with `BuildTool` to denote the specific build tool a
-/// package is built with. A `BuildToolVersion` wraps a `Version` (that is guaranteed to have a
-/// `PackageRelease`) and an `Architecture`.
+/// [`BuildToolVersion`] is used in conjunction with [`BuildTool`] to denote the specific build tool
+/// a package is built with.
+/// [`BuildToolVersion`] distinguishes between two types of representations:
+///
+/// - the one used by [makepkg], which relies on [`MinimalVersion`]
+/// - and the one used by [pkgctl] (devtools), which relies on [`FullVersion`] and the
+///   [`Architecture`] of the build tool.
+///
+/// For more information refer to the `buildtoolver` keyword in [BUILDINFOv2].
 ///
 /// ## Examples
 /// ```
 /// use std::str::FromStr;
 ///
-/// use alpm_types::BuildToolVersion;
+/// use alpm_types::{Architecture, BuildToolVersion, FullVersion, MinimalVersion};
 ///
-/// assert!(BuildToolVersion::from_str("1-1-any").is_ok());
-/// assert!(BuildToolVersion::from_str("1").is_ok());
-/// assert!(BuildToolVersion::from_str("1-1").is_err());
-/// assert!(BuildToolVersion::from_str("1-1-foo").is_err());
+/// # fn main() -> testresult::TestResult {
+/// // Representation used by makepkg
+/// assert_eq!(
+///     BuildToolVersion::from_str("1.0.0")?,
+///     BuildToolVersion::Makepkg(MinimalVersion::from_str("1.0.0")?)
+/// );
+/// assert_eq!(
+///     BuildToolVersion::from_str("1:1.0.0")?,
+///     BuildToolVersion::Makepkg(MinimalVersion::from_str("1:1.0.0")?)
+/// );
+///
+/// // Representation used by pkgctl
+/// assert_eq!(
+///     BuildToolVersion::from_str("1.0.0-1-any")?,
+///     BuildToolVersion::DevTools {
+///         version: FullVersion::from_str("1.0.0-1")?,
+///         architecture: Architecture::from_str("any")?
+///     }
+/// );
+/// assert_eq!(
+///     BuildToolVersion::from_str("1:1.0.0-1-any")?,
+///     BuildToolVersion::DevTools {
+///         version: FullVersion::from_str("1:1.0.0-1")?,
+///         architecture: Architecture::from_str("any")?
+///     }
+/// );
+/// # Ok(())
+/// # }
 /// ```
+///
+/// [BUILDINFOv2]: https://alpm.archlinux.page/specifications/BUILDINFOv2.5.html
+/// [makepkg]: https://man.archlinux.org/man/makepkg.8
+/// [pkgctl]: https://man.archlinux.org/man/pkgctl.1
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, Serialize)]
-pub struct BuildToolVersion {
-    version: Version,
-    architecture: Option<Architecture>,
+pub enum BuildToolVersion {
+    /// The version representation used by [makepkg].
+    ///
+    /// [makepkg]: https://man.archlinux.org/man/makepkg.8
+    Makepkg(MinimalVersion),
+    /// The version representation used by [pkgctl] (devtools).
+    ///
+    /// [pkgctl]: https://man.archlinux.org/man/pkgctl.1
+    DevTools {
+        /// The (_full_ or _full with epoch_) version of the build tool.
+        version: FullVersion,
+        /// The architecture of the build tool.
+        architecture: Architecture,
+    },
 }
 
 impl BuildToolVersion {
-    /// Create a new BuildToolVersion
-    pub fn new(version: Version, architecture: Option<Architecture>) -> Self {
-        BuildToolVersion {
-            version,
+    /// Returns the optional [`Architecture`].
+    ///
+    /// # Note
+    ///
+    /// If `self` is a [`BuildToolVersion::Makepkg`] this method always returns [`None`].
+    pub fn architecture(&self) -> Option<Architecture> {
+        if let Self::DevTools {
+            version: _,
             architecture,
+        } = self
+        {
+            Some(*architecture)
+        } else {
+            None
         }
     }
 
-    /// Return a reference to the Architecture
-    pub fn architecture(&self) -> &Option<Architecture> {
-        &self.architecture
-    }
-
-    /// Return a reference to the Version
-    pub fn version(&self) -> &Version {
-        &self.version
+    /// Returns a [`Version`] that matches the underlying [`MinimalVersion`] or [`FullVersion`].
+    pub fn version(&self) -> Version {
+        match self {
+            Self::Makepkg(version) => Version::from(version),
+            Self::DevTools {
+                version,
+                architecture: _,
+            } => Version::from(version),
+        }
     }
 }
 
 impl FromStr for BuildToolVersion {
     type Err = Error;
-    /// Create an BuildToolVersion from a string and return it in a Result
+    /// Creates a [`BuildToolVersion`] from a string slice.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if
+    ///
+    /// - `s` contains no '-' and `s` is not a valid [`MinimalVersion`],
+    /// - or `s` contains at least one '-' and after splitting on the right most occurrence, either
+    ///   the left-hand side is not a valid [`FullVersion`] or the right hand side is not a valid
+    ///   [`Architecture`].
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        const VERSION_DELIMITER: char = '-';
-        match s.rsplit_once(VERSION_DELIMITER) {
-            Some((version, architecture)) => match Architecture::from_str(architecture) {
-                Ok(architecture) => Ok(BuildToolVersion {
-                    version: Version::with_pkgrel(version)?,
-                    architecture: Some(architecture),
-                }),
-                Err(err) => Err(err.into()),
-            },
-            None => Ok(BuildToolVersion {
-                version: Version::from_str(s)?,
-                architecture: None,
+        match s.rsplit_once('-') {
+            Some((version, architecture)) => Ok(BuildToolVersion::DevTools {
+                version: FullVersion::from_str(version)?,
+                architecture: Architecture::from_str(architecture)?,
             }),
+            None => Ok(BuildToolVersion::Makepkg(MinimalVersion::from_str(s)?)),
         }
     }
 }
 
 impl Display for BuildToolVersion {
-    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
-        if let Some(architecture) = &self.architecture {
-            write!(fmt, "{}-{}", self.version, architecture)
-        } else {
-            write!(fmt, "{}", self.version)
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Makepkg(version) => write!(f, "{version}"),
+            Self::DevTools {
+                version,
+                architecture,
+            } => write!(f, "{version}-{architecture}"),
         }
     }
 }
@@ -86,35 +147,70 @@ impl Display for BuildToolVersion {
 #[cfg(test)]
 mod tests {
     use rstest::rstest;
+    use testresult::TestResult;
 
     use super::*;
 
-    /// Ensure that valid buildtool version strings are parsed as expected.
+    /// Ensure that valid strings are correctly parsed as [`BuildToolVersion`] and invalid ones lead
+    /// to an [`Error`].
     #[rstest]
-    #[case(
+    #[case::devtools_full(
         "1.0.0-1-any",
-        BuildToolVersion::new(Version::from_str("1.0.0-1").unwrap(), Some(Architecture::from_str("any").unwrap())),
+        Ok(BuildToolVersion::DevTools{version: FullVersion::from_str("1.0.0-1")?, architecture: Architecture::from_str("any")?}),
     )]
-    #[case(
+    #[case::devtools_full_with_epoch(
         "1:1.0.0-1-any",
-        BuildToolVersion::new(Version::from_str("1:1.0.0-1").unwrap(), Some(Architecture::from_str("any").unwrap())),
+        Ok(BuildToolVersion::DevTools{version: FullVersion::from_str("1:1.0.0-1")?, architecture: Architecture::from_str("any")?}),
     )]
-    #[case(
+    #[case::makepkg_minimal(
         "1.0.0",
-        BuildToolVersion::new(Version::from_str("1.0.0").unwrap(), None),
+        Ok(BuildToolVersion::Makepkg(MinimalVersion::from_str("1.0.0")?)),
     )]
-    fn valid_buildtoolver_new(#[case] buildtoolver: &str, #[case] expected: BuildToolVersion) {
+    #[case::makepkg_minimal_with_epoch(
+        "1:1.0.0",
+        Ok(BuildToolVersion::Makepkg(MinimalVersion::from_str("1:1.0.0")?)),
+    )]
+    #[case::minimal_version_with_architecture("1.0.0-any",
+        Err(Error::ParseError(
+            "1.0.0\n^\nexpected alpm-pkgver string, followed by a '-' and an alpm-pkgrel string".to_string()
+        ))
+     )]
+    #[case::minimal_version_with_epoch_and_architecture(
+        "1:1.0.0-any",
+        Err(Error::ParseError(
+            "1:1.0.0\n  ^\nexpected alpm-pkgver string, followed by a '-' and an alpm-pkgrel string".to_string()
+        ))
+    )]
+    #[case::full_version_with_bogus_architecture("1.0.0-1-foo", Err(strum::ParseError::VariantNotFound.into()))]
+    fn valid_buildtoolver_new(
+        #[case] input: &str,
+        #[case] expected: Result<BuildToolVersion, Error>,
+    ) -> TestResult {
+        let parse_result = BuildToolVersion::from_str(input);
         assert_eq!(
-            BuildToolVersion::from_str(buildtoolver),
-            Ok(expected),
-            "Expected valid parse of buildtoolver '{buildtoolver}'"
+            parse_result, expected,
+            "Expected '{expected:?}' when parsing '{input}' but got '{parse_result:?}'"
         );
+
+        Ok(())
     }
 
-    /// Ensure that invalid buildtool version strings produce the respective errors.
+    /// Ensures that [`BuildToolVersion::from_str`] fails on invalid version strings with specific
+    /// errors.
     #[rstest]
-    #[case("1.0.0-any", Error::MissingComponent { component: "pkgrel" })]
-    #[case("1.0.0-1-foo", strum::ParseError::VariantNotFound.into())]
+    #[case::minimal_version_with_architecture(
+        "1.0.0-any",
+        Error::ParseError(
+            "1.0.0\n^\nexpected alpm-pkgver string, followed by a '-' and an alpm-pkgrel string".to_string()
+        )
+    )]
+    #[case::minimal_version_with_invalid_architecture(
+        "1.0.0-foo",
+        Error::ParseError(
+            "1.0.0\n^\nexpected alpm-pkgver string, followed by a '-' and an alpm-pkgrel string".to_string()
+        )
+    )]
+    #[case::full_version_with_invalid_architecture("1.0.0-1-foo", strum::ParseError::VariantNotFound.into())]
     fn invalid_buildtoolver_new(#[case] buildtoolver: &str, #[case] expected: Error) {
         assert_eq!(
             BuildToolVersion::from_str(buildtoolver),
