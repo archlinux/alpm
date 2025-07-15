52f9a4f7d26a343f4f0b759f7d7739fd9c07f98f
Arne Beer <contact@arne.beer>
2025-07-15 18:34:30 +0200

    refactor: Use winnow for packager info parsing
    Signed-off-by: Arne Beer <contact@arne.beer>

--- alpm-types/src/error.rs
+++ alpm-types/src/error.rs
@@ -175,7 +175,6 @@ mod tests {
     use rstest::rstest;
 
     use super::*;
-    use crate::openpgp::PACKAGER_REGEX;
 
     #[rstest]
     #[case(
@@ -201,14 +200,6 @@ mod tests {
         "Invalid integer (caused by InvalidDigit)",
         Error::InvalidInteger {
             kind: IntErrorKind::InvalidDigit
-        }
-    )]
-    #[case(
-        "Value '€i²' does not match the 'packager' regex: ^(?P<name>[\\w\\s\\-().]+) <(?P<email>.*)>$",
-        Error::RegexDoesNotMatch {
-            value: "€i²".to_string(),
-            regex_type: "packager".to_string(),
-            regex: PACKAGER_REGEX.to_string(),
         }
     )]
     #[case(
