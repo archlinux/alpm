a96cd1a3003ec37087acb020005835196c4659d6
David Runge <dvzrv@archlinux.org>
2025-07-26 13:42:06 +0200

    feat: Add `FakerootBackend` based on the `fakeroot` executable
    Fixes: https://gitlab.archlinux.org/archlinux/alpm/alpm/-/issues/184
    Signed-off-by: David Runge <dvzrv@archlinux.org>

--- alpm-rootless/tests/integration.rs
+++ alpm-rootless/tests/integration.rs
@@ -1,5 +1,53 @@
 //! Integration tests for [`alpm_rootless`].
 
+use log::{LevelFilter, debug};
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+
+/// Initializes a global [`TermLogger`].
+fn init_logger() {
+    if TermLogger::init(
+        LevelFilter::Debug,
+        Config::default(),
+        TerminalMode::Stderr,
+        ColorChoice::Auto,
+    )
+    .is_err()
+    {
+        debug!("Not initializing another logger, as one is initialized already.");
+    }
+}
+
+mod fakeroot {
+    use alpm_rootless::{FakerootBackend, FakerootOptions, RootlessBackend};
+    use testresult::TestResult;
+
+    use super::*;
+
+    /// Ensures that on a Linux-based system, the [`FakerootBackend`] can be used to run a
+    /// command ([whoami]) as root.
+    ///
+    /// [whoami]: https://man.archlinux.org/man/whoami.1
+    #[test]
+    #[cfg(target_os = "linux")]
+    fn fakerootbackend_run() -> TestResult {
+        init_logger();
+
+        let backend = FakerootBackend::new(FakerootOptions::default());
+
+        let output = backend.run(&["whoami"])?;
+        if !output.status.success() {
+            return Err(format!(
+                "The \"fakeroot\" call exited with a non-zero return code:\n{}",
+                String::from_utf8_lossy(&output.stderr)
+            )
+            .into());
+        }
+
+        assert_eq!("root\n", String::from_utf8_lossy(&output.stdout));
+        Ok(())
+    }
+}
+
 mod utils {
     use alpm_rootless::get_command;
     use testresult::TestResult;
