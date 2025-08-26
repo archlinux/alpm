1a4768e6b7b8efd07684ec4eb72bb8a4b855aef2
Arne Beer <contact@arne.beer>
2025-08-26 15:55:51 +0200

    feat: NoArchitecture lint
    Signed-off-by: Arne Beer <contact@arne.beer>

---
+++ alpm-lint/tests/lint_rule_tests/source_info/no_architecture.rs
@@ -0,0 +1,42 @@
+use alpm_lint::{
+    Resources,
+    config::LintRuleConfiguration,
+    lint_rules::source_info::no_architecture::NoArchitecture,
+};
+use alpm_srcinfo::SourceInfo;
+use alpm_types::Architecture;
+
+use crate::fixtures::default_source_info_v1;
+
+#[test]
+fn no_architecture_passes() -> testresult::TestResult {
+    let mut source_info = default_source_info_v1()?;
+    source_info.base.architectures = vec![Architecture::X86_64];
+
+    let resources = Resources::SourceInfo(SourceInfo::V1(source_info));
+    let config = LintRuleConfiguration::default();
+    let lint_rule = NoArchitecture::new_boxed(&config);
+    let mut issues = Vec::new();
+
+    lint_rule.run(&resources, &mut issues)?;
+
+    assert!(issues.is_empty(), "No lint issues should have been found");
+    Ok(())
+}
+
+#[test]
+fn no_architecture_fails() -> testresult::TestResult {
+    let mut source_info = default_source_info_v1()?;
+    source_info.base.architectures = vec![];
+
+    let resources = Resources::SourceInfo(SourceInfo::V1(source_info));
+    let config = LintRuleConfiguration::default();
+    let lint_rule = NoArchitecture::new_boxed(&config);
+    let mut issues = Vec::new();
+
+    lint_rule.run(&resources, &mut issues)?;
+
+    assert!(!issues.is_empty(), "A lint error should've been found.");
+    assert_eq!(issues[0].lint_rule, "source_info::no_architecture");
+    Ok(())
+}
