f159e2b878c98091e95ef3998ebc30f5b164bc1b
Jagoda Ślązak <jslazak@jslazak.com>
2025-09-19 09:21:11 +0200

    refactor(python): Use mypy strict mode and fix all reported issues
    Signed-off-by: Jagoda Ślązak <jslazak@jslazak.com>

--- python-alpm/tests/test_alpm_imports.py
+++ python-alpm/tests/test_alpm_imports.py
@@ -1,10 +1,10 @@
-def test_imports():
+def test_imports() -> None:
     """Test that all items can be imported without errors."""
     import alpm  # noqa: F401
     from alpm import alpm_types, alpm_srcinfo, type_aliases, ALPMError  # noqa: F401
 
 
-def test_type_aliases_imports():
+def test_type_aliases_imports() -> None:
     """Test that all type aliases can be imported without errors."""
     from alpm.type_aliases import (  # noqa: F401
         Checksum,
