b0a76d7eb6d363de1456472a0fbd174bad8aa6f2
Jagoda Ślązak <jslazak@jslazak.com>
2025-09-22 12:10:01 +0200

    feat: Add third batch of alpm-srcinfo Python bindings
    This batch adds:
    - `SourceInfoSchema`
    - `source_info_from_file`
    - `source_info_from_str`
    
    Signed-off-by: Jagoda Ślązak <jslazak@jslazak.com>

--- python-alpm/src/srcinfo/mod.rs
+++ python-alpm/src/srcinfo/mod.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 
 mod error;
+mod schema;
 mod source_info;
 
 #[pymodule(gil_used = false, name = "alpm_srcinfo", submodule)]
@@ -12,7 +13,15 @@ pub mod py_srcinfo {
     #[pymodule_export]
     use super::error::py_error;
     #[pymodule_export]
+    use super::schema::SourceInfoSchema;
+    #[pymodule_export]
+    use super::schema::py_schema;
+    #[pymodule_export]
     use super::source_info::py_source_info;
+    #[pymodule_export]
+    use super::source_info::source_info_from_file;
+    #[pymodule_export]
+    use super::source_info::source_info_from_str;
     #[pymodule_export]
     use super::source_info::v1::SourceInfoV1;
     #[pymodule_export]
@@ -23,6 +32,7 @@ pub mod py_srcinfo {
         let modules = PyModule::import(m.py(), "sys")?.getattr("modules")?;
         modules.set_item("alpm.alpm_srcinfo.error", m.getattr("error")?)?;
         modules.set_item("alpm.alpm_srcinfo.source_info", m.getattr("source_info")?)?;
+        modules.set_item("alpm.alpm_srcinfo.schema", m.getattr("schema")?)?;
         Ok(())
     }
 }
--- python-alpm/src/srcinfo/source_info/mod.rs
+++ python-alpm/src/srcinfo/source_info/mod.rs
@@ -1,8 +1,10 @@
+use alpm_common::MetadataFile;
 use pyo3::prelude::*;
+
+use crate::srcinfo::schema::SourceInfoSchema;
 
 pub mod v1;
 
-#[allow(dead_code)]
 #[derive(FromPyObject, IntoPyObject)]
 pub enum SourceInfo {
     V1(v1::SourceInfoV1),
@@ -16,10 +18,36 @@ impl From<alpm_srcinfo::SourceInfo> for SourceInfo {
     }
 }
 
+#[pyfunction]
+#[pyo3(signature = (s, schema = None))]
+pub fn source_info_from_str(
+    s: &str,
+    schema: Option<SourceInfoSchema>,
+) -> Result<SourceInfo, crate::srcinfo::error::Error> {
+    let schema: Option<alpm_srcinfo::SourceInfoSchema> = schema.map(From::from);
+    let inner = alpm_srcinfo::SourceInfo::from_str_with_schema(s, schema)?;
+    Ok(inner.into())
+}
+
+#[pyfunction]
+#[pyo3(signature = (path, schema = None))]
+pub fn source_info_from_file(
+    path: std::path::PathBuf,
+    schema: Option<SourceInfoSchema>,
+) -> Result<SourceInfo, crate::srcinfo::error::Error> {
+    let schema: Option<alpm_srcinfo::SourceInfoSchema> = schema.map(From::from);
+    let inner = alpm_srcinfo::SourceInfo::from_file_with_schema(&path, schema)?;
+    Ok(inner.into())
+}
+
 #[pymodule(gil_used = false, name = "source_info", submodule)]
 pub mod py_source_info {
     use pyo3::prelude::*;
 
+    #[pymodule_export]
+    use super::source_info_from_file;
+    #[pymodule_export]
+    use super::source_info_from_str;
     #[pymodule_export]
     use super::v1::py_v1;
 
