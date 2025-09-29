4874bdfde96f88f79c241e57ef2f3f0e466aaad4
Jagoda Ślązak <jslazak@jslazak.com>
2025-09-29 12:45:43 +0200

    feat!: Remove `compression` module from `alpm-package`
    Functionality of `alpm_compress::compression` is now handled
    by `alpm-compress` crate.
    
    BREAKING CHANGE: Removes `compression` module and its reexported
      contents from the public API.
    
    Signed-off-by: Jagoda Ślązak <jslazak@jslazak.com>

--- alpm-package/src/lib.rs
+++ alpm-package/src/lib.rs
@@ -1,23 +1,11 @@
 #![doc = include_str!("../README.md")]
 
-pub mod compression;
 pub mod config;
 pub mod error;
 pub mod input;
 pub mod package;
 mod scriptlet;
 
-pub use compression::{
-    Bzip2CompressionLevel,
-    CompressionAlgorithm,
-    CompressionDecoder,
-    CompressionEncoder,
-    CompressionSettings,
-    GzipCompressionLevel,
-    XzCompressionLevel,
-    ZstdCompressionLevel,
-    ZstdThreads,
-};
 pub use config::{OutputDir, PackageCreationConfig};
 pub use error::Error;
 pub use input::{InputDir, PackageInput};
