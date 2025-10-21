af15bd441471ea25e3e3d23781dacd57d04e5e44
Orhun Parmaksız <orhun@archlinux.org>
2025-10-21 18:41:12 +0300

    feat: Localize error messages for alpm-pkginfo
    Signed-off-by: Orhun Parmaksız <orhun@archlinux.org>

---
+++ alpm-pkginfo/locales/en-US/error.ftl
@@ -0,0 +1,23 @@
+error-io-path = I/O error at path { $path } while { $context }:
+  { $source }
+
+error-io-read = Read error while { $context }:
+  { $source }
+
+error-deserialize = Failed to deserialize PKGINFO file:
+  { $source }
+
+error-extra-data-empty = Extra data field is specified without any value.
+
+error-first-extra-data-not-pkgtype = The first extra data definition does not specify "pkgtype".
+
+error-no-input-file = No input file given.
+
+error-json = JSON error:
+  { $source }
+
+error-invalid-variant = Invalid variant ({ $variant }).
+
+error-missing-extra-data = Extra data is missing.
+
+error-unsupported-schema = Unsupported schema version: { $version }.
--- alpm-pkginfo/src/error.rs
+++ alpm-pkginfo/src/error.rs
@@ -1,19 +1,28 @@
 use std::{path::PathBuf, string::FromUtf8Error};
 
-/// The Error that can occur when working with PKGINFO files
+use fluent_i18n::t;
+
+/// The Error that can occur when working with PKGINFO files.
 #[derive(Debug, thiserror::Error)]
 #[non_exhaustive]
 pub enum Error {
     /// ALPM type error
-    #[error("ALPM type parse error: {0}")]
+    #[error(transparent)]
     AlpmType(#[from] alpm_types::Error),
 
-    /// IO error
-    #[error("I/O error at path {0:?} while {1}:\n{2}")]
+    /// IO path error
+    #[error("{msg}", msg = t!("error-io-path", {
+        "path" => .0,
+        "context" => .1,
+        "source" => .2.to_string()
+    }))]
     IoPathError(PathBuf, &'static str, std::io::Error),
 
     /// I/O error while reading a buffer.
-    #[error("Read error while {context}:\n{source}")]
+    #[error("{msg}", msg = t!("error-io-read", {
+        "context" => context,
+        "source" => source.to_string()
+    }))]
     IoReadError {
         /// The context in which the error occurred.
         ///
@@ -23,39 +32,43 @@ pub enum Error {
         source: std::io::Error,
     },
 
-    /// UTF-8 parse error
+    /// UTF-8 parse error.
     #[error(transparent)]
     InvalidUTF8(#[from] FromUtf8Error),
 
     /// An [`alpm_parsers::custom_ini::Error`].
-    #[error("Failed to deserialize PKGINFO file:\n{0}")]
-    DeserializeError(#[from] alpm_parsers::custom_ini::Error),
+    #[error("{msg}", msg = t!("error-deserialize", { "source" => source.to_string() }))]
+    DeserializeError {
+        /// The deserialization error source.
+        #[from]
+        source: alpm_parsers::custom_ini::Error,
+    },
 
     /// An extra data field specified without any value.
-    #[error("Extra data field is specified without any value")]
+    #[error("{msg}", msg = t!("error-extra-data-empty"))]
     ExtraDataEmpty,
 
     /// The first extra data field does not specify "pkgtype".
-    #[error("The first extra data definition does not specify \"pkgtype\"")]
+    #[error("{msg}", msg = t!("error-first-extra-data-not-pkgtype"))]
     FirstExtraDataNotPkgType,
 
-    /// No input file given
-    #[error("No input file given.")]
+    /// No input file given.
+    #[error("{msg}", msg = t!("error-no-input-file"))]
     NoInputFile,
 
-    /// JSON error
-    #[error("JSON error: {0}")]
+    /// JSON error.
+    #[error("{msg}", msg = t!("error-json", { "source" => .0.to_string() }))]
     Json(#[from] serde_json::Error),
 
-    /// An invalid enum variant
-    #[error("Invalid variant ({0})")]
+    /// An invalid enum variant.
+    #[error("{msg}", msg = t!("error-invalid-variant", { "variant" => .0.to_string() }))]
     InvalidVariant(#[from] strum::ParseError),
 
-    /// Extra data is missing
-    #[error("Extra data is missing.")]
+    /// Extra data is missing.
+    #[error("{msg}", msg = t!("error-missing-extra-data"))]
     MissingExtraData,
 
-    /// Unsupported schema version
-    #[error("Unsupported schema version: {0}")]
+    /// Unsupported schema version.
+    #[error("{msg}", msg = t!("error-unsupported-schema", { "version" => .0 }))]
     UnsupportedSchemaVersion(String),
 }
