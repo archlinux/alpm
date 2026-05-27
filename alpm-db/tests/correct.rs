4d4928a23d54e80a1d606ae40d87c77cc4b46416
Arne Beer <contact@arne.beer>
2026-05-27 16:38:06 +0200

    refactor: Move alpm-db files inline tests to insta integration tests
    While at it, I "upgraded" the tests to be roundtrip tests.
    
    Signed-off-by: Arne Beer <contact@arne.beer>

---
+++ alpm-db/tests/correct.rs
@@ -0,0 +1,44 @@
+//! Integration tests to ensure correct [alpm-db-files] and [alpm-db-desc] files roundtrip
+//! through their parsed representation, JSON serialisation and string serialisation.
+//!
+//! [alpm-db-files]: https://alpm.archlinux.page/specifications/alpm-db-files.5.html
+//! [alpm-db-desc]: https://alpm.archlinux.page/specifications/alpm-db-desc.5.html
+
+use std::{fs::read_to_string, path::PathBuf, str::FromStr};
+
+use alpm_db::files::DbFiles;
+use insta::assert_snapshot;
+use rstest::rstest;
+use testresult::TestResult;
+
+/// Each `*.files` file in `tests/correct/files` is expected to parse successfully, serialize to
+/// json and roundtrip to the same string.
+///
+/// The test works as follows:
+/// 1. Parse the input as [`DbFiles`] and serialize it as JSON.
+/// 2. Compared the JSON against a snapshot in `tests/correct_snapshots/files`.
+/// 3. Render the parsed [`DbFiles`] back to a string and ensure that the output is identical to the
+///    original input.
+#[rstest]
+fn correct_files(#[files("tests/correct/files/*.files")] case: PathBuf) -> TestResult {
+    let input = read_to_string(&case)?;
+    let name = case
+        .file_stem()
+        .and_then(|s| s.to_str())
+        .unwrap_or("unknown_case");
+
+    let files = DbFiles::from_str(&input)?;
+
+    let json = serde_json::to_string_pretty(&files)?;
+    insta::with_settings!({
+        description => format!("{name} DbFiles JSON representation."),
+        snapshot_path => "correct_snapshots/files",
+        prepend_module_to_snapshot => false,
+    }, {
+        assert_snapshot!(name, json);
+    });
+
+    assert_eq!(files.to_string(), input);
+
+    Ok(())
+}
